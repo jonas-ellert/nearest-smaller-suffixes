f39e311b28fe8f0eeab1a63cf9d80bbb61d8b385
Jonas Ellert <jonas.ellert@tu-dortmund.de>
2019-10-28 15:32:30 +0100

    update auto formatter

--- auto-format.sh
+++ auto-format.sh
@@ -2,3 +2,4 @@
 find include/ -regex '.*\.\(cpp\|hpp\|cc\|cxx\)' -exec clang-format -style=file -i {} \;
 find benchmark/src/ -regex '.*\.\(cpp\|hpp\|cc\|cxx\)' -exec clang-format -style=file -i {} \;
 find benchmark/test/ -regex '.*\.\(cpp\|hpp\|cc\|cxx\)' -exec clang-format -style=file -i {} \;
+find benchmark/include/ -regex '.*\.\(cpp\|hpp\|cc\|cxx\)' -exec clang-format -style=file -i {} \;
